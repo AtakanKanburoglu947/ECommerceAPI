80fd1eab78638626a0330f249e9cd8acabd00011
AtakanKanburoglu947 <kanburogluatakn@gmail.com>
2023-09-06 11:05:40 +0300

    Added Core Layer and models

---
+++ ECommerceCore/Models/ShoppingCart.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceCore.Models
+{
+    public class ShoppingCart
+    {
+        public int Id { get; set; }
+        public IEnumerable<Product> Products { get; set; } 
+    }
+}
---
+++ ECommerceCore/Models/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceCore.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public int ShoppingCartId { get; set; }
+        public ShoppingCart ShoppingCart { get; set; }
+    }
+}
