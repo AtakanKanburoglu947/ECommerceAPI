7519f88a3abdf0875df9862f5fb10b3f18124025
AtakanKanburoglu947 <kanburogluatakn@gmail.com>
2023-09-06 15:04:46 +0300

    Added Repository Layer, installed FrameworkCore, FrameworkCore Design, FrameworkCore SqlServer, FrameworkCore Tools packages to Repository Layer , connected Repository Layer to Core layer and added AppDbContext

---
+++ ECommerceRepository/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using ECommerceCore.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceRepository
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Product> Products { get; set; } 
+        public DbSet<ShoppingCart> ShoppingCarts { get; set;}
+    }
+}
